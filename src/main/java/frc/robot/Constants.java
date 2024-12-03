42425c3762b967bb24fa871b59be2229d3132b8e
Andrew Chen <bobopop787@gmail.com>
2024-12-03 04:40:57 -0500

    Lesson 1
    Basic Flywheel

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+public class Constants {
+    public static class ShooterConstants {
+        public static final int kShooterLowerMotorCanID = 13;
+		public static final int kShooterUpperMotorCanID = 14;
+        public static final int kShooterIndexMotorCanID = 12;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,50 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ShooterConstants;
+
+public class Shooter extends SubsystemBase {
+    private double speed;
+    private CANSparkMax shooterLowerMotor;
+    private CANSparkMax shooterUpperMotor;
+    private CANSparkMax shooterIndexMotor;
+
+    public Shooter() {
+        speed = 0;
+        shooterLowerMotor = new CANSparkMax(ShooterConstants.kShooterLowerMotorCanID, MotorType.kBrushless);
+        shooterUpperMotor = new CANSparkMax(ShooterConstants.kShooterUpperMotorCanID, MotorType.kBrushless);
+        shooterIndexMotor = new CANSparkMax(ShooterConstants.kShooterIndexMotorCanID, MotorType.kBrushless);
+        
+        shooterLowerMotor.restoreFactoryDefaults();
+        shooterUpperMotor.restoreFactoryDefaults();
+        shooterIndexMotor.restoreFactoryDefaults();
+
+        shooterLowerMotor.burnFlash();
+        shooterUpperMotor.burnFlash();
+        shooterIndexMotor.burnFlash();
+    }
+
+    public void shoot() {
+        shooterLowerMotor.setVoltage(-3);
+        shooterUpperMotor.setVoltage(3);
+        shooterIndexMotor.setVoltage(-3);
+    }
+
+    public void rest() {
+        shooterLowerMotor.setVoltage(0);
+        shooterUpperMotor.setVoltage(0);
+        shooterIndexMotor.setVoltage(0);
+    }
+
+    public Command runShoot() {
+        return runOnce(() -> shoot());
+    }
+    
+    public Command runRest() {
+        return runOnce(() -> rest());
+    }
+}
